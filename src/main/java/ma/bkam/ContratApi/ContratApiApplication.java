768138893d265bd3906716e2e0561743754799f1
Alae Eddine Manar <alaeeddine@mobiblanc.world>
2022-03-30 18:26:09 +0200

    main commit

---
+++ src/main/java/ma/bkam/ContratApi/ContratApiApplication.java
@@ -0,0 +1,13 @@
+package ma.bkam.ContratApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/bkam/ContratApi/ContratApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bkam.ContratApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
